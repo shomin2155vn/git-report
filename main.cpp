0d9c318960f8c288694d308ab3f08acb83938cdb
unknown <phuong.hoanghcmut106@hcmut.edu.vn>
2026-05-11 10:36:07 +0700

    Update comment

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int sum (int a, int b) {
     cout << a + b;
     cout << "This is the sum function." << endl;
+    cout << "This function takes two integers and returns their sum." << endl;
     return 1;
 }
 
