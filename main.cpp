7ae03f70f594c8bbe7ed037ee4a6bacdd1148a37
unknown <phuong.hoanghcmut106@hcmut.edu.vn>
2026-05-11 11:01:27 +0700

    Change file

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int sum (int a, int b) {
-    cout << "This is the sum function." << endl;
     cout << "This function takes two integers and returns their sum." << endl;
     cout << "The sum of " << a << " and " << b << " is: " << a + b << endl;
     return 1;
