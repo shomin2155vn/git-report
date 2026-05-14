6c73ad3846c8d69112997f5ac36a492d476ba814
unknown <phuong.hoanghcmut106@hcmut.edu.vn>
2026-05-14 09:31:37 +0700

    Add comment

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int sum (int a, int b) {
     cout << "This function takes two integers and returns their sum." << endl;
     cout << "The sum of " << a << " and " << b << " is: " << a + b << endl;
+    // Commenting out the return statement to demonstrate a function without a return value
     return 1;
 }
 
