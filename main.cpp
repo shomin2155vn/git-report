662612ddca7fe3681a5c0e98a0e6d8cafadfc8b7
unknown <phuong.hoanghcmut106@hcmut.edu.vn>
2026-05-14 09:47:32 +0700

    Change func sum

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int sum (int a, int b) {
     cout << "This function takes two integers and returns their sum." << endl;
     cout << "The sum of " << a << " and " << b << " is: " << a + b << endl;
-    // Commenting out the return statement to demonstrate a function without a return value
     return 1;
 }
 
